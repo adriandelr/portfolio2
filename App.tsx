bf62f5a27aa0e9fba553a8f84c3cc6c8373cc52c
adriandelr <adrian.delr@gmail.com>
2024-11-22 08:32:37 +0700

    Add web support, fix safe area

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
 
 import Animated, {
   interpolate,
   useAnimatedRef,
   useAnimatedStyle,
-  useScrollViewOffset,
+  useSharedValue,
+  useAnimatedScrollHandler,
 } from "react-native-reanimated";
 import useCachedResources from "./app/hooks/useCachedResources";
 
@@ -14,10 +15,14 @@ const { width } = Dimensions.get("window");
 const IMG_HEIGHT = 300;
 
 const Portfolio = () => {
-  useCachedResources();
+  const isLoadingComplete = useCachedResources();
 
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
-  const scrollOffset = useScrollViewOffset(scrollRef);
+  const scrollOffset = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    scrollOffset.value = event.contentOffset.y;
+  });
 
   const imageAnimatedStyle = useAnimatedStyle(() => {
     return {
@@ -40,33 +45,43 @@ const Portfolio = () => {
     };
   });
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Animated.ScrollView ref={scrollRef} scrollEventThrottle={17}>
-        {/* Top Parallax Image */}
-        <Animated.Image
-          source={require("./assets/images/logos/logo-pp.png")}
-          style={[styles.image, imageAnimatedStyle]}
-        />
-
-        {/* Parallax Content */}
-        <View style={{ height: 700, backgroundColor: "whitesmoke" }}>
-          <Text
-            style={{
-              color: "slategrey",
-              fontSize: 17,
-              fontWeight: "300",
-              textAlign: "center",
-              fontFamily: "proxima-regular",
-            }}
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.container}>
+          <Animated.ScrollView
+            ref={scrollRef}
+            onScroll={scrollHandler}
+            scrollEventThrottle={17}
           >
-            Cross-Platform Digital Portfolio
-          </Text>
-        </View>
-      </Animated.ScrollView>
-      <StatusBar style="light" backgroundColor="black" />
-    </SafeAreaView>
-  );
+            {/* Top Parallax Image */}
+            <Animated.Image
+              source={require("./assets/images/logos/logo-pp.png")}
+              style={[styles.image, imageAnimatedStyle]}
+            />
+
+            {/* Parallax Content */}
+            <View style={{ height: 700, backgroundColor: "whitesmoke" }}>
+              <Text
+                style={{
+                  color: "slategrey",
+                  fontSize: 17,
+                  fontWeight: "300",
+                  textAlign: "center",
+                  fontFamily: "proxima-regular",
+                }}
+              >
+                Cross-Platform Digital Portfolio
+              </Text>
+            </View>
+          </Animated.ScrollView>
+          <StatusBar style="light" backgroundColor="black" />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    );
+  }
 };
 export default Portfolio;
 
--- app/hooks/useCachedResources.ts
+++ app/hooks/useCachedResources.ts
@@ -4,6 +4,8 @@ import * as SplashScreen from "expo-splash-screen";
 import * as React from "react";
 
 export default function useCachedResources() {
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+
   // Load any resources or data that we need prior to rendering the app
   React.useEffect(() => {
     async function loadResourcesAndDataAsync() {
@@ -19,10 +21,13 @@ export default function useCachedResources() {
         // We might want to provide this error information to an error reporting service
         console.warn(e);
       } finally {
+        setLoadingComplete(true);
         SplashScreen.hideAsync();
       }
     }
 
     loadResourcesAndDataAsync();
   }, []);
+
+  return isLoadingComplete;
 }
