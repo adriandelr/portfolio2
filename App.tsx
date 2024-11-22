d4d6ddc75977f66a215d4413ce226bb1fee057d9
adriandelr <adrian.delr@gmail.com>
2024-11-23 04:16:42 +0700

    Add Custom Splash Screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
+
+import React, { FunctionComponent, useState } from "react";
+import AnimatedSplashScreen from "./app/AnimatedSplashScreen";
 
 import Animated, {
   interpolate,
@@ -11,15 +15,18 @@ import Animated, {
 } from "react-native-reanimated";
 import useCachedResources from "./app/hooks/useCachedResources";
 
+SplashScreen.hide();
+
 const { width } = Dimensions.get("window");
 const IMG_HEIGHT = 300;
 
-const Portfolio = () => {
+const Portfolio: FunctionComponent = () => {
+  const [animationCompleted, setAnimationComplete] = useState<Boolean>(false);
+  const changeAnimationStatus = (param: Boolean) => setAnimationComplete(param);
   const isLoadingComplete = useCachedResources();
 
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
   const scrollOffset = useSharedValue(0);
-
   const scrollHandler = useAnimatedScrollHandler((event) => {
     scrollOffset.value = event.contentOffset.y;
   });
@@ -45,43 +52,41 @@ const Portfolio = () => {
     };
   });
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <SafeAreaView style={styles.container}>
-          <Animated.ScrollView
-            ref={scrollRef}
-            onScroll={scrollHandler}
-            scrollEventThrottle={17}
-          >
-            {/* Top Parallax Image */}
-            <Animated.Image
-              source={require("./assets/images/logos/logo-pp.png")}
-              style={[styles.image, imageAnimatedStyle]}
-            />
+  return !animationCompleted && !isLoadingComplete ? (
+    <AnimatedSplashScreen onFinish={changeAnimationStatus} />
+  ) : (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Animated.ScrollView
+          ref={scrollRef}
+          onScroll={scrollHandler}
+          scrollEventThrottle={17}
+        >
+          {/* Top Parallax Image */}
+          <Animated.Image
+            source={require("./assets/images/logos/logo-pp.png")}
+            style={[styles.image, imageAnimatedStyle]}
+          />
 
-            {/* Parallax Content */}
-            <View style={{ height: 700, backgroundColor: "whitesmoke" }}>
-              <Text
-                style={{
-                  color: "slategrey",
-                  fontSize: 17,
-                  fontWeight: "300",
-                  textAlign: "center",
-                  fontFamily: "proxima-regular",
-                }}
-              >
-                Cross-Platform Digital Portfolio
-              </Text>
-            </View>
-          </Animated.ScrollView>
-          <StatusBar style="light" backgroundColor="black" />
-        </SafeAreaView>
-      </SafeAreaProvider>
-    );
-  }
+          {/* Parallax Content */}
+          <View style={{ height: 700, backgroundColor: "whitesmoke" }}>
+            <Text
+              style={{
+                color: "slategrey",
+                fontSize: 17,
+                fontWeight: "300",
+                textAlign: "center",
+                fontFamily: "proxima-regular",
+              }}
+            >
+              Cross-Platform Digital Portfolio
+            </Text>
+          </View>
+        </Animated.ScrollView>
+        <StatusBar style="light" backgroundColor="black" />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 };
 export default Portfolio;
 
