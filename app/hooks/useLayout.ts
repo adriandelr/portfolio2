7292b3ae5d32bdfd2c331ed477eeb232bd0f5438
adriandelr <adrian.delr@gmail.com>
2024-12-23 07:00:55 +0800

    Convert layout to responsive screen hook

---
+++ app/hooks/useLayout.ts
@@ -0,0 +1,27 @@
+import { Dimensions } from "react-native";
+import { useState, useEffect } from "react";
+
+const windowWidth = Dimensions.get("window").width;
+
+export const isSmallScreen = (): boolean => {
+  const breakpoint = 1024;
+  return handleWindowChange(breakpoint);
+};
+
+export const isSmallerScreen = (): boolean => {
+  const breakpoint = 420;
+  return handleWindowChange(breakpoint);
+};
+
+const handleWindowChange = (breakpoint: number): boolean => {
+  const [width, setWidth] = useState(windowWidth <= breakpoint);
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener("change", ({ window }) => {
+      setWidth(window.width <= breakpoint);
+    });
+    return () => subscription?.remove();
+  });
+
+  return width;
+};
