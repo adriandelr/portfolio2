6dfda38557b5b02913c507481253ac450fd2303d
adriandelr <adrian.delr@gmail.com>
2024-12-23 07:01:34 +0800

    Refactor platform

--- app/utils/Platform.ts
+++ app/utils/Platform.ts
@@ -1,5 +1,9 @@
 import { Platform } from "react-native";
 
-export const isPlatformWeb = (): boolean => Platform.OS === "web";
-export const isPlatformAndroid = (): boolean => Platform.OS === "android";
-export const isPlatformIOS = (): boolean => Platform.OS === "ios";
+const platform = Platform.OS;
+
+export default {
+  isWeb: platform === "web",
+  isAndroid: platform === "android",
+  isIOS: platform === "ios",
+};
