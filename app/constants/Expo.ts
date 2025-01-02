b4038d565ed67d210cbe7f8f3140ad73535dffcb
adriandelr <adrian.delr@gmail.com>
2025-01-03 02:57:38 +0800

    Update expo url

--- app/constants/Expo.ts
+++ app/constants/Expo.ts
@@ -3,4 +3,4 @@ export const expoDevURL = "https://expo.dev",
   expoPlayStoreURL =
     "https://play.google.com/store/apps/details?id=host.exp.exponent",
   expoURL =
-    "exp://u.expo.dev/b6005ca1-f261-4b9d-8b27-918a03477a19/group/674d2933-7cf1-4b13-bc50-d11c629eab22";
+    "exp://u.expo.dev/b6005ca1-f261-4b9d-8b27-918a03477a19/group/75408b56-7912-4bee-8dce-cf2c3c508978";
